0dbd689fa1553b6f58c8669bf6800042b21e930e
zeruFili <140791955+zeruFili@users.noreply.github.com>
2024-04-28 23:44:26 -1200

    Update Product.jsx

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -28,9 +28,9 @@ const CreateTable = () => {
       const response = await fetch("http://127.0.0.1:3000/api/v1/tours", {
         headers: {
           "Content-Type": "application/json",
-          "access-token": "V3cwsYnnMRPi_S2nKFVEhA",
-          client: "mE-XbkK2ZoS-nHR60fht0g",
-          uid: "usergguopuigadoni@example.com",
+          "access-token": "",
+          client: "",
+          uid: "",
         },
       });
 
